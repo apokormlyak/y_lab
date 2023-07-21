b068b3047d2d34abf4048eaf217d30c5c918fd89
Ваше Имя <you@example.com>
2023-07-21 07:41:50 +0300

    first commit

---
+++ backend/models.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from django.db import models
+
+app = FastAPI()
+
+
+# @app.get('/')
+# def read_root():
+#     html_content = '<h2>Hello METANIT.COM!</h2>'
+#     return HTMLResponse(content=html_content)
+
+
+class Menu(models.Model):
+    submenu = models.CharField('Разделы меню', max_length=300, db_index=True)
+
+    class Meta:
+        verbose_name = 'Меню'
+
+
+class Submenu(models.Model):
+    menu = models.ForeignKey(Menu.submenu, verbose_name='Подменю', on_delete=models.CASCADE)
+    meal = models.CharField('Блюда', max_length=300, db_index=True)
+
+    class Meta:
+        verbose_name = 'Разделы меню'
+
+
+class Meal(models.Model):
+    name = models.CharField('Блюдо', max_length=300, db_index=True)
+    submenu = models.ForeignKey(Submenu.meal, verbose_name='Подменю', on_delete=models.CASCADE)
+    price = models.DecimalField('Стоимость', null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'Блюда'
---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+# @app.get('/')
+# def read_root():
+#     html_content = '<h2>Hello METANIT.COM!</h2>'
+#     return HTMLResponse(content=html_content)
+
+
+class Menu:
+    def __init__(self):
+        pass
+
+
+class Submenu:
+    pass
+
+
+class Meal:
+    pass
