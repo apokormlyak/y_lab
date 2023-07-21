8d1568a7a9e311e2346c9a2a13fd351b4815c6f3
Ваше Имя <you@example.com>
2023-07-21 08:08:51 +0300

    add Base

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from django.db import models
+from base import Base
 
 app = FastAPI()
 
@@ -11,14 +12,14 @@ app = FastAPI()
 #     return HTMLResponse(content=html_content)
 
 
-class Menu(models.Model):
+class Menu(Base, models.Model):
     submenu = models.CharField('Разделы меню', max_length=300, db_index=True)
 
     class Meta:
         verbose_name = 'Меню'
 
 
-class Submenu(models.Model):
+class Submenu(Base, models.Model):
     menu = models.ForeignKey(Menu.submenu, verbose_name='Подменю', on_delete=models.CASCADE)
     meal = models.CharField('Блюда', max_length=300, db_index=True)
 
@@ -26,7 +27,7 @@ class Submenu(models.Model):
         verbose_name = 'Разделы меню'
 
 
-class Meal(models.Model):
+class Meal(Base, models.Model):
     name = models.CharField('Блюдо', max_length=300, db_index=True)
     submenu = models.ForeignKey(Submenu.meal, verbose_name='Подменю', on_delete=models.CASCADE)
     price = models.DecimalField('Стоимость', null=True, blank=True)
